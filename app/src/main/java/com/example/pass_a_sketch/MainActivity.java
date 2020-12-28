495adb7897655d4382030731287cf0fddef21be3
yena816 <yena816@gmail.com>
2020-12-28 13:02:39 -0500

    Initial commit

---
+++ app/src/main/java/com/example/pass_a_sketch/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.pass_a_sketch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
